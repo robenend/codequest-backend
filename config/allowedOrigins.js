c2d012b55412d1359fac69e4ea75f9c9d971ba15
robenend <roberaendale2@gmail.com>
2025-03-28 11:41:49 +0300

    origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,3 @@
-const allowedOrigins = [
-	"http://localhost:5173",
-	"http://localhost:5000",
-	"https://admin.waystrad.com",
-	"https://admin.waystrad.com",
-	"https://waystrad.com",
-	"https://www.waystrad.com",
-	"http://82.112.241.229:5000",
-	"https://82.112.241.229",
-];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5000"];
 
 export default allowedOrigins;
